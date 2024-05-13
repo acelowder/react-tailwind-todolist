ff30c785bc06d4abf26cc7ba2740b76ea3aabbc5
Ace <ace.lowder@gmail.com>
2024-05-13 11:56:35 -0700

    Add todo tasks

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import TodoTask from './TodoTask'
 
 export default function TodoList() {
+  let todos = [
+    'This is your task list',
+    'Click on a task to complete it',
+    'Drag and drop to change the order'
+  ]
+
   return (
-    <div>
-      to do list
-    </div>
+    <ul className='flex flex-col'>
+      {todos.map((todo, index) => {
+        console.log(todo);
+        return (
+          <TodoTask key={index} todo={todo} />
+        )
+      })}
+    </ul>
   )
 }
---
+++ src/components/TodoTask.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TodoTask({ todo }) {
+  return (
+    <li className='task-item'>{todo}</li>
+  )
+}
