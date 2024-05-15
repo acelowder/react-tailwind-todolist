b4dae6ccf5961106c09f6ae7831076ffa7821425
Ace <ace.lowder@gmail.com>
2024-05-15 13:21:35 -0700

    Change netlify settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
 import TodoApp from './components/TodoApp';
 
 function App() {
 	return (
 		<main className="flex justify-center">
-			<Routes>
-				<Route path="/todo" element={<TodoApp />} />
-			</Routes>
+			<TodoApp />
 		</main>
 	);
 }
